b04eb8669e501211da7b8f646fabca534a289c82
root <facundoalarcon93@gmail.com>
2020-10-31 00:35:48 +0000

    script

--- script.sh
+++ script.sh
@@ -3,7 +3,6 @@ docker system prune -af
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
-docker rmi $(docker images -q)
 docker rmi $(docker images -a -q) -f
 
 docker volume rm $(docker volume ls -qf dangling=true)
